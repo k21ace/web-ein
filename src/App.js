12f1da0bcf0f657bdc1a8ff7d0df17c1d41f2082
k21ace <wahb34311@gmail.com>
2024-11-11 22:25:51 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Now standing at the starting line.
         </p>
         <a
           className="App-link"
